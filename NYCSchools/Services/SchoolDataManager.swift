f323a742e38b828e3882b86f343a634af5213c3e
Tanzeem <tanzeem1812@gmail.com>
2023-03-16 00:36:49 -0500

    MVVM Sample code

--- NYCSchools/Services/SchoolDataManager.swift
+++ NYCSchools/Services/SchoolDataManager.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// If more time will be given, proper wrappers(functions) can be introduced in this class to access following data objects (Set and Array)
+
 class SchoolDataManager{
     var schoolsExtraData = Set<SchoolExtraDataModel>()
     var schoolsData = [SchoolDataModel]()
