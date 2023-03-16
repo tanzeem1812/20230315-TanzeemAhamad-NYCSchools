578fcd67825862368ea81bf39e84d20f7070ce09
Tanzeem <tanzeem1812@gmail.com>
2023-03-16 01:57:05 -0500

    MVVM Sample code

--- NYCSchools/Services/SchoolDataManager.swift
+++ NYCSchools/Services/SchoolDataManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // If more time will be given, proper wrappers(functions) can be introduced in this class to access following data objects (Set and Array)
-// Business logic to fetch the data from the data source or makeing API call can be moved in this class
+// Business logic to fetch the data from the data source or makeing API call can be moved from view model to this class
 
 class SchoolDataManager{
     var schoolsExtraData = Set<SchoolExtraDataModel>()
